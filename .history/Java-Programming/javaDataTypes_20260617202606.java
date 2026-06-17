d6f582a4a52a7de6e3f59f1050a1cc7bdc3e2f00
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-17 20:26:38 +0530

    message

---
+++ .history/Java-Programming/javaDataTypes_20260617202606.java
@@ -0,0 +1,23 @@
+public class javaDataTypes {
+    public static void main(String[] args) {
+        // Declaring variables of different data types
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        byte myByte = 100;            // Byte
+        short myShort = 5000;          // Short
+        long myLong = 15000000000L;    // Long
+
+        // Printing the values of the variables
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + myLetter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Long: " + myLong);
+    }
+}
