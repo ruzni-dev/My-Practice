76ebddaca281df448a8d7804a4b0831b1c9d62a5
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-17 20:11:31 +0530

    message

---
+++ .history/Java-Programming/java-comments_20260616235959.java
@@ -0,0 +1,18 @@
+package Java-Programming;
+
+public class java-comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("This line will be executed.");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("This line will also be executed.");
+
+        // You can also use comments to explain your code
+        int sum = 5 + 10; // This adds 5 and 10
+        System.out.println("The sum is: " + sum);
+    }
+}
---
+++ Java-Programming/javaComments.java
@@ -0,0 +1,16 @@
+public class javaComments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("This line will be executed.");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("This line will also be executed.");
+
+        // You can also use comments to explain your code
+        int sum = 5 + 10; // This adds 5 and 10
+        System.out.println("The sum is: " + sum);
+    }
+}
