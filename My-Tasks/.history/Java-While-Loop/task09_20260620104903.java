1a0cc8e8acb92130d0b8d1f4a338f68c3c5cb9d3
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-20 11:29:16 +0530

    message

--- My-Tasks/.history/Java-While-Loop/task09_20260620104903.java
+++ My-Tasks/.history/Java-While-Loop/task09_20260620104903.java
@@ -1,18 +1,15 @@
-// Generate Fibonacci series up to n terms.
-import java.util.Scanner; // Import the Scanner class to read user input
+import java.util.Scanner;
 
 public class task09 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Create a Scanner object to read input from the user
-
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the number of Fibonacci terms to generate: ");
-        int terms = scanner.nextInt(); // Read the number of terms for which the Fibonacci series will be generated
+        int terms = scanner.nextInt();
 
         int count = 0;
         int first = 0;
         int second = 1;
 
-        // Print the Fibonacci series up to the specified number of terms
         System.out.println("Fibonacci series up to " + terms + " terms:");
         while (count < terms) {
             System.out.print(first);
@@ -26,8 +23,7 @@ public class task09 {
             count++;
         }
 
-        //  Print a new line after the series is printed
         System.out.println();
-        scanner.close(); // Close the scanner to prevent resource leaks
+        scanner.close();
     }
 }
