e8f6affe56bb24fe5dab1e4c2bb2b69c8eaa604a
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-20 20:21:42 +0530

    message

--- .history/My-Tasks/Java-For-Loop-&-Nested-For-Loop/task01_20260620201849.java
+++ .history/My-Tasks/Java-For-Loop-&-Nested-For-Loop/task01_20260620201849.java
@@ -2,7 +2,7 @@
 public class task01 {
     public static void main(String[] args) {
 
-        // Output: Pattern Printing
+        // Output:
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
