a98d6ddf949295a4b7c2ca0a48c998453b24c857
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-20 20:18:32 +0530

    message

--- .history/My-Tasks/Java-For-Loop-&-Nested-For-Loop/task05_20260620201629.java
+++ .history/My-Tasks/Java-For-Loop-&-Nested-For-Loop/task05_20260620201629.java
@@ -2,14 +2,9 @@
 
 public class task05 {
     public static void main(String[] args) {
-        int totalRows = 5;
 
         // Upper part
-        for (int i = 1; i <= totalRows; i++) {
-            int spaces = totalRows - i;
-            for (int s = 1; s <= spaces; s++) {
-                System.out.print(" ");
-            }
+        for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
@@ -17,11 +12,7 @@ public class task05 {
         }
 
         // Lower part
-        for (int i = totalRows - 1; i >= 1; i--) {
-            int spaces = totalRows - i;
-            for (int s = 1; s <= spaces; s++) {
-                System.out.print(" ");
-            }
+        for (int i = 4; i >= 1; i--) {
             for (int j = 1; j <= (2 * i - 1); j++) {
                 System.out.print("*");
             }
