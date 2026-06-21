46cc64b76e0580528049dc87bf5818611509cdab
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-21 19:18:01 +0530

    message

---
+++ Java-Programming/.history/booleans_20260621191656.java
@@ -0,0 +1,22 @@
+public class boolean {
+    public static void main(String[] args) {
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+
+        System.out.println("Is Java fun? " + isJavaFun);
+        System.out.println("Is fish tasty? " + isFishTasty);
+
+        // Using boolean in conditions
+        if (isJavaFun) {
+            System.out.println("I love programming in Java!");
+        } else {
+            System.out.println("I don't like programming in Java.");
+        }
+
+        if (isFishTasty) {
+            System.out.println("I enjoy eating fish.");
+        } else {
+            System.out.println("I don't enjoy eating fish.");
+        }
+    }
+}
---
+++ Java-Programming/.history/math_20260621191424.java
@@ -0,0 +1,33 @@
+public class math {
+    public static void main(String[] args) {
+        // Basic Math Operations
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Basic Math Operations:");
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        // Using Math class
+        double x = 16.0;
+        double y = 3.0;
+
+        System.out.println("\nUsing Math class:");
+        System.out.println("Square root of " + x + ": " + Math.sqrt(x));
+        System.out.println("Power of " + x + " and " + y + ": " + Math.pow(x, y));
+        System.out.println("Maximum of " + x + " and " + y + ": " + Math.max(x, y));
+        System.out.println("Minimum of " + x + " and " + y + ": " + Math.min(x, y));
+        System.out.println("Absolute value of -5: " + Math.abs(-5));
+        System.out.println("Random number between 0 and 100: " + (Math.random() * 100));
+
+        // Rounding, Ceiling, and Floor
+        double z = 5.7;
+        System.out.println("\nRounding, Ceiling, and Floor:");
+        System.out.println("Rounded value of " + z + ": " + Math.round(z));
+        System.out.println("Ceiling of " + z + ": " + Math.ceil(z));
+        System.out.println("Floor of " + z + ": " + Math.floor(z));
+    }
+}
