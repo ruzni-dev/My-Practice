df736a140aad720a419410a323f87adc4ab6782f
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-21 19:22:34 +0530

    message

---
+++ Java-Programming/forEachLoop.java
@@ -0,0 +1,10 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date", "Elderberry"};
+
+        System.out.println("Fruits in the array:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
