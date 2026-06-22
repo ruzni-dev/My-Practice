7945b0acb1b7c56be3657b2f4db55d50902b9ddc
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-22 20:16:37 +0530

    message\

---
+++ Java-Programming/.history/LargestOfTwoNumbers_20260622201625.java
@@ -0,0 +1,10 @@
+public class LargestOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 25;
+
+        if (a > b)
+            System.out.println("A is largest");
+        else
+            System.out.println("B is largest");
+    }
+}
