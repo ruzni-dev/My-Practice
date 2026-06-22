3bc0552f41414fd49fd2addd4d849b86343ccfa9
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-22 20:30:55 +0530

    message\

---
+++ Java-Programming/AreaOfCircle.java
@@ -0,0 +1,9 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        double r = 5;
+
+        double area = 3.14 * r * r;
+
+        System.out.println("Area = " + area);
+    }
+}
