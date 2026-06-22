1b4d2185b7dac1218656a6a79c1ae42ec99a3a40
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-22 19:31:01 +0530

    message

--- My-Tasks/.history/Java-Arrays/task01_20260622192935.java
+++ My-Tasks/.history/Java-Arrays/task01_20260622192935.java
@@ -2,17 +2,15 @@
 
 public class task01 {
     public static void main(String[] args) {
-        int[] arr = {10, 20, 5, 30, 15}; // Initialize the array
-        int largest = arr[0]; // Assume the first element is the largest
+        int[] arr = {10, 20, 5, 30, 15};
+        int largest = arr[0];
 
-        // Iterate through the array to find the largest element
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > largest) {
                 largest = arr[i];
             }
         }
 
-        // Print the largest element
         System.out.println("The largest element in the array is: " + largest);
     }    
 }
