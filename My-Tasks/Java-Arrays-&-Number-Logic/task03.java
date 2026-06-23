a1001a7f1901c0b6693e479ead6414a78b700220
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-23 16:44:09 +0530

    message

---
+++ My-Tasks/Java-Arrays-&-Number-Logic/task03.java
@@ -0,0 +1,21 @@
+// Count even and odd numbers in an array.
+
+public class task03 {
+    public static void main(String[] args) {
+        int[] numbers = {12, 45, 7, 10, 24, 33};
+
+        int even = 0;
+        int odd = 0;
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even numbers count: " + even);
+        System.out.println("Odd numbers count: " + odd);
+    }
+}
