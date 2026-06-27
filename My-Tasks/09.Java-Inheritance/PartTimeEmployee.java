0c89fda251d5317cba418f413eb1dc5ccd800bdb
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-27 19:55:15 +0530

    message

---
+++ My-Tasks/09.Java-Inheritance/PartTimeEmployee.java
@@ -0,0 +1,28 @@
+// Child class: PartTimeEmployee
+
+public class PartTimeEmployee extends Employee {
+
+    private int hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, int hoursWorked, double hourlyRate) {
+        this.name = name;
+        this.id = id;
+        this.hoursWorked = hoursWorked;
+        this.hourlyRate = hourlyRate;
+    }
+
+    // Salary calculation for part-time employee
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+
+    public void displayPartTimeDetails() {
+        displayInfo();
+        System.out.println("Employment Type: Part-Time");
+        System.out.println("Hours Worked   : " + hoursWorked);
+        System.out.println("Hourly Rate    : ₹" + hourlyRate);
+        System.out.println("Total Salary    : ₹" + calculateSalary());
+        System.out.println("=================================");
+    }
+}
