97d8dbdbbfa73dc775e4a097d8dc68289cab9f9e
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-06-30 21:23:53 +0530

    message

---
+++ Java-Programming/.history/OOP/DefaultConstructor_20260630212119.java
@@ -0,0 +1,12 @@
+public class DefaultConstructor {
+    class Demo {
+        int x;
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Demo d = new Demo();
+            System.out.println(d.x);
+        }
+    }
+}
---
+++ Java-Programming/.history/OOP/DefaultConstructor_20260630212229.java
@@ -0,0 +1,9 @@
+public class DefaultConstructor {
+    class Demo {
+        int x;
+    }
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        System.out.println(d.x);
+    }
+}
