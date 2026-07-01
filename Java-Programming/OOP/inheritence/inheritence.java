bf48cd10b38164c55d34a5e71aada1c181d44372
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-07-01 10:55:04 +0530

    message

---
+++ Java-Programming/OOP/inheritence/inheritence.java
@@ -0,0 +1,31 @@
+// This is a simple Java program that demonstrates inheritance. The `Animal` class is the parent class, and the `Dog` class is the child class that inherits from `Animal`.
+
+class Animal {
+    void eat() {
+        System.out.println("Animal eats food");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class inheritence {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        cat Cat = new cat();
+
+        dog.eat();
+        dog.bark();
+        Cat.eat();
+        Cat.meow();
+    }
+}
