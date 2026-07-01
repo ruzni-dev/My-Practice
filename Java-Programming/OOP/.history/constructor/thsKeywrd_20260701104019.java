d740e59df4e4fe9f5f98f443ac898fff15b94931
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-07-01 10:42:52 +0530

    message

--- Java-Programming/OOP/.history/constructor/thsKeywrd_20260701104019.java
+++ Java-Programming/OOP/.history/constructor/thsKeywrd_20260701104019.java
@@ -1,4 +1,4 @@
-// This is a simple Java program that demonstrates the use of constructors. The `Student` class has a constructor that initializes the `name` and `age` attributes. In the `main` method, we create an instance of the `Student` class and print out the student's name and age.
+package constructor;
 
 public class thsKeywrd {
     public static void main(String[] args) {
