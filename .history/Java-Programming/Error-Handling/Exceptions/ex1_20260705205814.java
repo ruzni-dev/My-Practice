edf7b60d35c910be4fdb6198536fdb544d6a9cff
ruzni-dev <185385872+ruzni-dev@users.noreply.github.com>
2026-07-05 20:58:32 +0530

    message

---
+++ .history/Java-Programming/Error-Handling/Exceptions/ex1_20260705205814.java
@@ -0,0 +1,13 @@
+public class ex1 {
+    public static void main(String[] args) {
+        try {
+            int x = 10 / 0;
+        }
+        catch(Exception e) {
+            System.out.println("Exception handled");
+        }
+        finally {
+            System.out.println("Always runs");
+        }
+    }
+}
